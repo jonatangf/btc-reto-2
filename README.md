2bb90b81b6414dcf76655ae6f6069b3cb071aac5
Jonatan <jntngf@gmail.com>
2021-12-14 16:00:09 +0000

    Add column for cart

---
+++ README.md
@@ -0,0 +1,10 @@
+# Reto 2
+
+## Public URL
+This project is public and can be found at:
+
+[https://jonatangf.github.io/btc-reto-2/](https://jonatangf.github.io/btc-reto-2/)
+
+## TODO
+* Handle drop article out of cart div. Div stays with red border.
+* Group amount of the same articles in cart, and show x2, x3, x4, etc.
--- js/drag_and_drop.js
+++ js/drag_and_drop.js
@@ -2,43 +2,52 @@ const allowDrop = (ev) => {
     ev.preventDefault();
 }
 
-const drag = (ev) => {
+const dragArticle = (ev) => {
     ev.dataTransfer.setData("id", ev.target.id);
     document.getElementById("cart-container").className = "cart-focused col-sm-4";
 }
 
-const dropOnCart = (ev) => {
+const dropArticleOnCart = (ev) => {
     console.log("drop has been called");
     ev.preventDefault();
     let elementId = ev.dataTransfer.getData("id");
     let element = document.getElementById(elementId);
     let parentElement = element.parentElement
 
-    let elementPrice = parentElement.getElementsByClassName("showcase-item-price").item(0).textContent;
+    let elementPrice = parentElement.getElementsByClassName("showcase-item-price").item(0).textContent
+        .replaceAll(",", ".");
     let elementName = parentElement.getElementsByClassName("showcase-item-name").item(0).textContent;
 
     let articleNameElement = document.createElement("div");
-    articleNameElement.className = "col-sm-8";
+    articleNameElement.className = "col-sm-6";
     articleNameElement.textContent = elementName;
+
+    let amountElement = document.createElement("div");
+    amountElement.className = "col-sm-2";
+    amountElement.textContent = "x1";
 
     let priceElement = document.createElement("div");
     priceElement.className = "col-sm-2";
     priceElement.textContent = elementPrice;
 
-    let priceCurrency = document.createElement("div");
-    priceCurrency.className = "col-sm-2";
-    priceCurrency.textContent = "€";
+    let totalElementPrice = document.createElement("div");
+    totalElementPrice.className = "col-sm-2";
+    totalElementPrice.textContent = elementPrice;
 
     let cartElement = document.createElement("div");
     cartElement.className = "row";
     cartElement.appendChild(articleNameElement);
+    cartElement.appendChild(amountElement);
     cartElement.appendChild(priceElement);
-    cartElement.appendChild(priceCurrency);
+    cartElement.appendChild(totalElementPrice);
 
     let total_elements = parseInt(document.getElementById("total-elements").textContent) + 1;
     document.getElementById("total-elements").textContent = total_elements.toString();
 
-    document.getElementById("total-price").textContent = (parseFloat(document.getElementById("total-price").textContent) + parseFloat(elementPrice)).toFixed(2);
+    let currentTotalPrice = parseFloat(document.getElementById("total-price").textContent);
+    let parsedElementPrice = parseFloat(elementPrice);
+    currentTotalPrice = currentTotalPrice + parsedElementPrice;
+    document.getElementById("total-price").textContent = currentTotalPrice.toFixed(2);
 
     document.getElementById("cart").appendChild(cartElement);
     document.getElementById("cart-container").className = "cart-unfocused col-sm-4";
